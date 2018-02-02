3ed26903e7f5bb40ad83cc3caae43732442329bb
Tkachenko AI <alexfisher13@yandex.ru>
2018-02-02 11:49:55 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  tasks = [
+    {title: 'Хлеб', status: true},
+    {title: 'Молоко', status: true},
+    {title: 'Картошка', status: true},
+    {title: 'Лук', status: true},
+    {title: 'Морковь', status: true},
+    {title: 'Капуста', status: true},
+    {title: 'Щавель', status: true},
+  ];
+
+  toggle(task) {
+    task.status = !task.status;
+  }
+
+  remove(i) {
+    this.tasks.splice(i, 1);
+  }
+
+  add(newTitle) {
+    this.tasks.push({
+      title: newTitle,
+      status: true
+    })
+  }
 }
